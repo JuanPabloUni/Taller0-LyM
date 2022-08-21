07415fd19727c5763d91cb4fe13ea853f4876c29
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2022-08-21 18:53:45 -0500

    Commit

--- Parser.py
+++ Parser.py
@@ -1,8 +1,18 @@
 alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od", "fi", "go", "GORP", "(", ")", "{", "}", ",", ".", ";", "north", "south", "east", "west", "right", "left", "front", "back", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if", "else"]
 
-file = open("Text.txt", "r")
-for line in file:
-  print(line)
+def Parser():
+  path = input("Porfavor digite el path del archivo: ")
+  file = open(path, "r")
+  for line in file:
+    print(line)
+
 
 # file1 = open("Text.txt", "r")
 # print(file1.read())
+
+# file = open("Text.txt", "r")
+# for line in file:
+#   print(line)
+
+print("Bienvenido al parser de programa de Java")
+Parser()
