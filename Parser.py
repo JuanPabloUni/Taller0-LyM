9ddab6c08abd691c3630a5b23b708e3c7414015f
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2022-08-21 19:28:45 -0500

    Methods

--- Parser.py
+++ Parser.py
@@ -1,12 +1,18 @@
 alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od", "fi", "go", "GORP", "(", ")", "{", "}", ",", ".", ";", "north", "south", "east", "west", "right", "left", "front", "back", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if", "else"]
 
-def Parser() -> bool:
+def Parser()->bool:
   path = input("\nPorfavor digite el path del archivo: ")
   file = open(path, "r")
   lineas=[]
   for line in file:
     lineas.append(line)
 
+def verificador(lineas:list)->bool:
+  for lineap in lineas:
+    comparador(lineap)
+   
+def comparador(lineacomparada: str)->bool:
+  pass
 
 # file1 = open("Text.txt", "r")
 # print(file1.read())
@@ -17,8 +23,3 @@ def Parser() -> bool:
 
 print("\nBienvenido al parser de programas de Java\n")
 Parser()
-def verificador(lineas:list)->bool
-  for lineap in lista:
-    comparador(lineap)
-   
-def comparador(lineacomparada: string)->bool
