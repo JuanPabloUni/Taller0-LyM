c49e713e4d6e4780bfad1849fce6427de0378ec1
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2022-08-21 19:26:28 -0500

    A

--- Parser.py
+++ Parser.py
@@ -1,10 +1,12 @@
 alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od", "fi", "go", "GORP", "(", ")", "{", "}", ",", ".", ";", "north", "south", "east", "west", "right", "left", "front", "back", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if", "else"]
 
-def Parser():
+def Parser() -> bool:
   path = input("\nPorfavor digite el path del archivo: ")
   file = open(path, "r")
+  print("\n")
   for line in file:
     print(line)
+  print("\n")
 
 
 # file1 = open("Text.txt", "r")
