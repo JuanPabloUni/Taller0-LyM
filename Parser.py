a99caea697c02d595c1aba574b4b553a344a3050
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2022-08-24 16:45:13 -0500

    Cambios

--- Parser.py
+++ Parser.py
@@ -17,143 +17,107 @@ def Parser()->bool:
   file.close()
   lineas.strip()
 
-  def recorrer(lineas:)->str:
-     palabra = ""
-     noecontrado=""
-     for indice in range(len(lineas)):
-       caracter = lineas[indice]
-       palabra+caracter
-       for indicea in range(len(metodos)):
-         token = cadena[indicea]
-         if palabra in token:
-           encontrado.add()
-           bool=comparadormetodos(palabra, indice, )
-           palabra = ""
-         else:
-           noencontrado.add()
+def recorrer(lineas:str)->str:
+  palabra = ""
+  noecontrado=""
+  for indice in range(len(lineas)):
+    caracter = lineas[indice]
+    palabra+caracter
+    for indicea in range(len(metodos)):
+      token = cadena[indicea]
+      if palabra in token:
+        encontrado.add()
+        bool=comparadormetodos(palabra, indice, )
+        palabra = ""
+      else:
+        noencontrado.add()
               
-  def comparadormetodos(palabra:str, indice:int, lineas:str)->bool:
+def comparadormetodos(palabra:str, indice:int, lineas:str)->bool:
+  valor_verdad=False
+  if palabra == "drop":
+    valor_verdad=comparardrop(lineas:str, indice:int)
+  elif palabra == "walkm":
+    valor_verdad=compararwalkmultiple(lineas:str, indice:int)    
+  elif palabra == "jump":
+    valor_verdad=compararjump(lineas:str, indice:int)
+  elif palabra == "jumpTo":
+    valor_verdad=compararjumpTo(lineas:str, indice:int) 
+  elif palabra == "veer":
+    valor_verdad=compararveer(lineas:str, indice:int)
+  elif palabra == "look":
+    valor_verdad=compararlook(lineas:str, indice:int)
+  elif palabra == "grab":
+    valor_verdad=comparargrab(lineas:str, indice:int)
+  elif palabra == "get":
+    valor_verdad=compararget(lineas:str, indice:int)
+  elif palabra == "free":
+    valor_verdad=compararfree(lineas:str, indice:int)      
+  elif palabra == "pop":
+    valor_verdad=compararpop(lineas:str, indice:int)
+  elif palabra == "isfacing":
+    valor_verdad=compararisfacing(lineas:str, indice:int)
+  elif palabra == "isValid":
+    valor_verdad=compararisValid(lineas:str, indice:int)
+  elif palabra == "canWalk":
+    valor_verdad=compararcanwalkmultiple(lineas:str, indice:int)
+  elif palabra == "not":
+    valor_verdad=compararnot(lineas:str, indice:int)
+  else:
     valor_verdad=False
-    if palabra == "drop":
-      valor_verdad=comparardrop(lineas:str, indice:int)
-    elif palabra == "walkm":
-      valor_verdad=compararwalkmultiple(lineas:str, indice:int)    
-    elif palabra == "jump":
-      valor_verdad=compararjump(lineas:str, indice:int)
-    elif palabra == "jumpTo":
-      valor_verdad=compararjumpTo(lineas:str, indice:int) 
-    elif palabra == "veer":
-      valor_verdad=compararveer(lineas:str, indice:int)
-    elif palabra == "look":
-      valor_verdad=compararlook(lineas:str, indice:int)
-    elif palabra == "grab":
-      valor_verdad=comparargrab(lineas:str, indice:int)
-    elif palabra == "get":
-      valor_verdad=compararget(lineas:str, indice:int)
-    elif palabra == "free":
-      valor_verdad=compararfree(lineas:str, indice:int)      
-    elif palabra == "pop":
-      valor_verdad=compararpop(lineas:str, indice:int)
-    elif palabra == "isfacing":
-      valor_verdad=compararisfacing(lineas:str, indice:int)
-    elif palabra == "isValid":
-      valor_verdad=compararisValid(lineas:str, indice:int)
-    elif palabra == "canWalk":
-      valor_verdad=compararcanwalkmultiple(lineas:str, indice:int)
-    elif palabra == "not":
-      valor_verdad=compararnot(lineas:str, indice:int)
-    else:
-      valor_verdad=False
-    return valor_verdad  
+  return valor_verdad  
    
-  
-
-
-  
-  def comparardrop(lineas:str, indice:int)->tuple:
-    pass
-  def compararwalkmultiple(lineas:str, indice:int)->tuple:
-    pass
-  def compararjump(lineas:str, indice:int)->tuple:
-    pass
-  def compararjumpTo(lineas:str, indice:int)->tuple:
-    pass
-  def compararveer(lineas:str, indice:int)->tuple:
-    pass
-  def compararlook(lineas:str, indice:int)->tuple:
-    pass
-  def comparargrab(lineas:str, indice:int)->tuple:
-    pass
-  def compararget(lineas:str, indice:int)->tuple:
-    pass
-  def compararfree(lineas:str, indice:int)->tuple:
-    pass
-  def compararpop(lineas:str, indice:int)->tuple:
-    pass
-  def compararisfacing(lineas:str, indice:int)->tuple:
-    pass
-  def compararisValid(lineas:str, indice:int)->tuple:
-    pass
-  def compararcanwalkmultiple(lineas:str, indice:int)->tuple:
-    pass
-  def compararnot(lineas:str, indice:int)->tuple:
-    pass
-  
+def comparardrop(lineas:str, indice:int)->tuple:
+  pass
+def compararwalkmultiple(lineas:str, indice:int)->tuple:
+  pass
+def compararjump(lineas:str, indice:int)->tuple:
+  pass
+def compararjumpTo(lineas:str, indice:int)->tuple:
+  pass
+def compararveer(lineas:str, indice:int)->tuple:
+  pass
+def compararlook(lineas:str, indice:int)->tuple:
+  pass
+def comparargrab(lineas:str, indice:int)->tuple:
+  pass
+def compararget(lineas:str, indice:int)->tuple:
+  pass
+def compararfree(lineas:str, indice:int)->tuple:
+  pass
+def compararpop(lineas:str, indice:int)->tuple:
+  pass
+def compararisfacing(lineas:str, indice:int)->tuple:
+  pass
+def compararisValid(lineas:str, indice:int)->tuple:
+  pass
+def compararcanwalkmultiple(lineas:str, indice:int)->tuple:
+  pass
+def compararnot(lineas:str, indice:int)->tuple:
+  pass      
       
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      # estructuras si no esta sacar el nombre e ir a posicion y evaluar
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    
-                """ 
-            else:
-              noencontrado=palabra
-              for indicea in range(len(alfabeto)):
-                token = cadena[indicea]
-                if token in noencontrado:
-                  noencontrado=""
-                     """
-
-                  
-        
-        
-        
-
-          
-        
-"""  
-def verificador(lineas:list)->bool:
-  for lineap in lineas:
-    comparador(lineap)
+#       # estructuras si no esta sacar el nombre e ir a posicion y evaluar
+#                 """ 
+#             else:
+#               noencontrado=palabra
+#               for indicea in range(len(alfabeto)):
+#                 token = cadena[indicea]
+#                 if token in noencontrado:
+#                   noencontrado=""
+#                      """     
+# """  
+# def verificador(lineas:list)->bool:
+#   for lineap in lineas:
+#     comparador(lineap)
    
-def comparador(lineacomparada: str)->bool:
-  palabra=""
-  for letra in lineacomparada:
-    palabra+= letra
-    if(palabra in metodos):
-      if palabra = "if":
-        compararif()
-"""
+# def comparador(lineacomparada: str)->bool:
+#   palabra=""
+#   for letra in lineacomparada:
+#     palabra+= letra
+#     if(palabra in metodos):
+#       if palabra = "if":
+#         compararif()
+# """
   
 # file1 = open("Text.txt", "r")
 # print(file1.read())
