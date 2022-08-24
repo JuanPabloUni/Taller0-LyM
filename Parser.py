1f1dc544d0335e6f897876f3e5fcb84556be3e2f
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2022-08-24 02:36:21 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,12 +1,53 @@
 alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od", "fi", "go", "GORP", "(", ")", "{", "}", ",", ".", ";", "north", "south", "east", "west", "right", "left", "front", "back", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if", "else"]
-metodos=
+metodos=[]
+encontrado =[]
+noencontrado=[]
 def Parser()->bool:
   path = input("\nPorfavor digite el path del archivo: ")
   file = open(path, "r")
+  """
+  codigo anterior, el que esta fuera ahora lee todo el archivo en string
   lineas=[]
   for line in file:
     lineas.append(line)
+   """
+  lineas = file.read().replace("\n", " ")
+  file.close()
+  lineas.strip()
 
+   def recorrer(lineas:)->str:
+      palabra = ""
+      noecontrado=""
+      for indice in range(len(lineas)):
+        caracter = cadena[indice]
+        palabra+caracter
+        for indicea in range(len(alfabeto)):
+          token = cadena[indicea]
+            if palabra in token:
+              encontrado.add()
+              palabra = ""
+            else:
+              noencontrado.add()
+     
+   
+  
+                """ 
+            else:
+              noencontrado=palabra
+              for indicea in range(len(alfabeto)):
+                token = cadena[indicea]
+                if token in noencontrado:
+                  noencontrado=""
+                     """
+
+                  
+        
+        
+        
+
+          
+        
+"""  
 def verificador(lineas:list)->bool:
   for lineap in lineas:
     comparador(lineap)
@@ -18,7 +59,8 @@ def comparador(lineacomparada: str)->bool:
     if(palabra in metodos):
       if palabra = "if":
         compararif()
-
+"""
+  
 # file1 = open("Text.txt", "r")
 # print(file1.read())
 
