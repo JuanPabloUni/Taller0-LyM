403e6331227405a29163acf4e08209b430c23c96
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2022-08-24 16:54:52 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,5 +1,5 @@
 alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od", "fi", "go", "GORP", "(", ")", "{", "}", ",", ".", ";", "north", "south", "east", "west", "right", "left", "front", "back", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if", "else"]
-metodos=["drop", "free", "walk", "PROC","canWalk", "do", "walk", "od", "fi", "go", "jump", "jumpTo", "veer", "look", "grab", "get", "pop", "if","GORP", "isfacing", "isValid", "canWalk", "not"]
+metodos=["drop", "walk", "jump", "jumpTo", "veer", "look", "grab", "get", "free", "pop", "isfacing", "isValid", "canWalk","not", "PROC", "do", "od", "fi", "go",  "if","GORP", ]
 encontrado =[]
 noencontrado=[]
 programas = []
