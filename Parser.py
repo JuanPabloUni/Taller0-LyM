8b83f8f6177f357db8c25171d0072506eabe4249
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2022-08-24 04:43:55 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -2,6 +2,8 @@ alfabeto = ["drop", "free", "walk", "var", "PROC", "canWalk", "do", "walk", "od"
 metodos=[]
 encontrado =[]
 noencontrado=[]
+programas = []
+
 def Parser()->bool:
   path = input("\nPorfavor digite el path del archivo: ")
   file = open(path, "r")
@@ -15,22 +17,99 @@ def Parser()->bool:
   file.close()
   lineas.strip()
 
-   def recorrer(lineas:)->str:
-      palabra = ""
-      noecontrado=""
-      for indice in range(len(lineas)):
-        caracter = cadena[indice]
-        palabra+caracter
-        for indicea in range(len(alfabeto)):
-          token = cadena[indicea]
-            if palabra in token:
-              encontrado.add()
-              palabra = ""
-            else:
-              noencontrado.add()
-     
+  def recorrer(lineas:)->str:
+     palabra = ""
+     noecontrado=""
+     for indice in range(len(lineas)):
+       caracter = cadena[indice]
+       palabra+caracter
+       for indicea in range(len(alfabeto)):
+         token = cadena[indicea]
+         if palabra in token:
+           encontrado.add()
+           bool=comparadormetodos(palabra, indice)
+           palabra = ""
+         else:
+           noencontrado.add()
+              
+  def comparadormetodos(palabra:str, indice:int)->bool:
+    valor_verdad=False
+    if palabra == "drop":
+      valor_verdad=comparardrop(lineas:string, indice:int)
+    elif palabra == "walkm":
+      valor_verdad=compararwalkmultiple(lineas:string, indice:int)    
+    elif palabra == "jump":
+      valor_verdad=compararjump(lineas:string, indice:int)
+    elif palabra == "jumpTo":
+      valor_verdad=compararjumpTo(lineas:string, indice:int) 
+    elif palabra == "veer":
+      valor_verdad=compararveer(lineas:string, indice:int)
+    elif palabra == "look":
+      valor_verdad=compararlook(lineas:string, indice:int)
+    elif palabra == "grab":
+      valor_verdad=comparargrab(lineas:string, indice:int)
+    elif palabra == "get":
+      valor_verdad=compararget(lineas:string, indice:int)
+    elif palabra == "free":
+      valor_verdad=compararfree(lineas:string, indice:int)      
+    elif palabra == "pop":
+      valor_verdad=compararpop(lineas:string, indice:int)
+    elif palabra == "isfacing":
+      valor_verdad=compararisfacing(lineas:string, indice:int)
+    elif palabra == "isValid":
+      valor_verdad=compararisValid(lineas:string, indice:int)
+    elif palabra == "canWalk":
+      valor_verdad=compararcanwalkmultiple(lineas:string, indice:int)
+    elif palabra == "not":
+      valor_verdad=compararnot(lineas:string, indice:int)
+    else:
+      valor_verdad=False
+    return valor_verdad  
    
   
+
+  def compararisfacing(lineas:string, indice:int)
+  def compararisValid(lineas:string, indice:int)
+  def compararcanwalkmultiple(lineas:string, indice:int)
+  def compararnot(lineas:string, indice:int)
+  
+  def comparardrop(lineas:string, indice:int)->bool:
+  def compararwalkmultiple(lineas:string, indice:int)->bool:
+  def compararjump(lineas:string, indice:int)->bool:
+  def compararjumpTo(lineas:string, indice:int)->bool:
+  def compararveer(lineas:string, indice:int)->bool:
+  def compararlook(lineas:string, indice:int)->bool:
+  def comparargrab(lineas:string, indice:int)->bool:
+  def compararget(lineas:string, indice:int)->bool:
+  def compararfree(lineas:string, indice:int)->bool:
+  def compararpop(lineas:string, indice:int)->bool:
+  
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      estructuras si no esta sacar el nombre e ir a posicion y evaluar
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
                 """ 
             else:
               noencontrado=palabra
